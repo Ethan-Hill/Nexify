0a97b8aa11b028b96e42ba12fa4f82374d3f00db
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-15 21:23:52 +0000

    Added progress bar

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withProgressBar = require("next-progressbar");
+
+module.exports = withProgressBar({
   functions: {
     "api/auth/[...nextauth]": {
       maxDuration: 30,
     },
   },
-};
+});
