921726ecc37c03dc3a25bd68693352470487b698
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-15 21:22:48 +0000

    Removed Pointless Config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   functions: {
     "api/auth/[...nextauth]": {
       maxDuration: 30,
