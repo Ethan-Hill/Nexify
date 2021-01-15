79e23364ea5826e4b572bbba897b43d02feefef6
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-15 21:39:17 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const withProgressBar = require("next-progressbar");
 
 module.exports = withProgressBar({
+  //...
+  resolve: {
+    modules: ["node_modules", "js"],
+    alias: {
+      "mapbox-gl": path.resolve("./node_modules/mapbox-gl/dist/mapbox-gl.js"),
+    },
+  },
+
   functions: {
     "api/auth/[...nextauth]": {
       maxDuration: 30,
