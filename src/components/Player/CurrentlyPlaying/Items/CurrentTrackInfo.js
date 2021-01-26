7f5b0e61bbfbdfaa825145bb0e75acf6d8a45be1
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-26 12:04:40 +0000

    Fixed info dialog

--- src/components/Player/CurrentlyPlaying/Items/CurrentTrackInfo.js
+++ src/components/Player/CurrentlyPlaying/Items/CurrentTrackInfo.js
@@ -12,12 +12,11 @@ export default function CurrentTrackInfo() {
     <div>
       <div className="absolute text-3xl top-3 right-3">
         <sl-theme name="dark">
-          <sl-tooltip content="Info" class="dialog-toggle">
-            <sl-icon-button
-              name="info-circle-fill"
-              label="Info"
-            ></sl-icon-button>
-          </sl-tooltip>
+          <sl-icon-button
+            name="info-circle-fill"
+            label="Info"
+            class="dialog-toggle"
+          ></sl-icon-button>
         </sl-theme>
       </div>
     </div>
--- src/components/Player/Dialog/TrackInfo.js
+++ src/components/Player/Dialog/TrackInfo.js
@@ -6,18 +6,20 @@ export default function TrackInfo({ track }) {
     <sl-theme name={theme}>
       <sl-dialog label="Dialog" class="dialog-overview">
         <sl-tab-group>
-          {track.item.album.artists.map((artist) => (
-            <sl-tab slot="nav" panel="general">
+          {track.item.artists.map((artist) => (
+            <sl-tab slot="nav" panel={artist.id} key={artist.id}>
               {artist.name}
             </sl-tab>
           ))}
 
-          <sl-tab-panel name="general">
-            This is the general tab panel.
-          </sl-tab-panel>
-          <sl-tab-panel name="custom">
-            This is the custom tab panel.
-          </sl-tab-panel>
+          {track.item.artists.map((artist) => (
+            <sl-tab-panel name={artist.id}>
+              <div className="flex flex-col">
+                <h1>Artist</h1>
+                {artist.name}
+              </div>
+            </sl-tab-panel>
+          ))}
         </sl-tab-group>
       </sl-dialog>
     </sl-theme>
