551a6c11625125b8e34de2544fc5cee20252165c
Ethan-Hill <ethanhilljay@outlook.com>
2021-01-28 12:23:01 +0000

    Update CurrentTrackImage.js

--- src/components/Player/CurrentlyPlaying/Items/CurrentTrackImage.js
+++ src/components/Player/CurrentlyPlaying/Items/CurrentTrackImage.js
@@ -16,8 +16,8 @@ export default function UserImage({ src }) {
       <img
         className="object-cover object-center h-56"
         src={"https://i.imgur.com/AtMhw8m.png"}
-        width="224px"
-        height="224px"
+        width="100%"
+        height="100%"
         alt="Current Song Img"
       />
     );
